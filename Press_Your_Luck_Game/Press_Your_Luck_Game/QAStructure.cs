0e7275a08dab20eb045f804bf1b63cd6cc4e1370
Anthony Enem <Anthony Enem>
2016-09-24 15:23:20 -0500

    Adding project files

---
+++ Press_Your_Luck_Game/Press_Your_Luck_Game/QAStructure.cs
@@ -0,0 +1,58 @@
+﻿//QAStructure.cs file
+//Class for Question and answer
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Press_Your_Luck_Game
+{
+    class QAStructure
+    {
+        //private data
+        private String question;
+        private String answer;
+
+        //constructor
+        public QAStructure()
+        {
+            question = "None";
+            answer = "None";
+        }
+
+        //parameterized constructor
+        public QAStructure(String question, String answer)
+        {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        //getter and setter for questio
+        public String Question
+        {
+            get
+            {
+                return question;
+            }
+            set
+            {
+                this.question = value;
+            }
+        }
+
+        //getter and setter for answer
+        public String Answer
+        {
+            get
+            {
+                return answer;
+            }
+            set
+            {
+                this.answer = value;
+            }
+        }
+    }
+}
