69e54e6340b4ac1012a9cdda9260650b90dac32a
Anthony Enem <Anthony Enem>
2016-09-24 14:55:55 -0500

    Adding project files

---
+++ Press_Your_Luck_Game/Press_Your_Luck_Game/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Press_Your_Luck_Game
+{
+    class Player
+    {
+        private int cash;
+        private int spins;
+
+        public Player()
+        {
+            cash = 0;
+            spins = 0;
+        }
+
+        public int Spins
+        {
+            get
+            {
+                return spins;
+            }
+
+            set
+            {
+                this.spins = value;
+            }
+        }
+
+        public int Cash
+        {
+            get
+            {
+                return cash;
+            }
+            set
+            {
+                this.cash = value;
+            }
+        }
+    }
+}
